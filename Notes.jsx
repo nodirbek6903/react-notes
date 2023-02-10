081baf86d4aa9c06b35db3c63132b4eb6ad3b07e
Nodirbek Umarov <nu2913634@gmail.com>
2023-02-10 20:01:34 +0300

    Add files via upload

---
+++ Notes.jsx
@@ -0,0 +1,60 @@
+import { CiSearch } from "react-icons/ci";
+import { MdClose } from "react-icons/md";
+// import dummy_notes from "../dummy_notes";
+import { Link } from "react-router-dom";
+import { BsPlusLg } from "react-icons/bs";
+import NoteItem from "../components/NoteItem";
+import { useEffect, useState } from "react";
+const Notes = ({ notes }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState(notes);
+
+  const handleSearch = () => {
+    setFilteredNotes(
+      notes.filter((note) => {
+        if (note.title.toLowerCase().match(text.toLocaleLowerCase())) {
+          return note;
+        }
+      })
+    );
+  };
+
+  useEffect(handleSearch, [text]);
+
+  return (
+    <section>
+      <header className="notes__header">
+        {!showSearch && <h2>My Notes</h2>}
+        {showSearch && (
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            autoFocus
+            placeholder="Keyword..."
+          />
+        )}
+        <button
+          className="btn"
+          onClick={() => setShowSearch((prevState) => !prevState)}
+        >
+          {showSearch ? <MdClose /> : <CiSearch />}
+        </button>
+      </header>
+      <div className="notes__container">
+        {filteredNotes.length == 0 && (
+          <p className="empty__notes">Note notes found.</p>
+        )}
+        {filteredNotes.map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
+      </div>
+      <Link to="/create-note" className="btn add__btn">
+        <BsPlusLg />
+      </Link>
+    </section>
+  );
+};
+
+export default Notes;
---
+++ dummy_notes.jsx
@@ -0,0 +1,61 @@
+const notes = [
+  {
+    id: 1,
+    title: "What is Lorem Ipsum?",
+    details:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    date: "Jan 6, 2023",
+  },
+  {
+    id: 2,
+    title: "Why do we use it?",
+    details:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    date: "Jan 6, 2023",
+  },
+  {
+    id: 3,
+    title: "Where does it come from?",
+    details:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum",
+    date: "Jan 6, 2023",
+  },
+  {
+    id: 4,
+    title: "Where can I get some?",
+    details:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the",
+    date: "Jan 6, 2023",
+  },
+  {
+    id: 5,
+    title: "The standard Lorem Ipsum passage, used since the 1500s",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    date: "Jan 6, 2023",
+  },
+  {
+    id: 6,
+    title:
+      "Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC",
+    details:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
+    date: "Jan 7, 2023",
+  },
+  {
+    id: 7,
+    title: "1914 translation by H. Rackham",
+    details:
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is",
+    date: "Jan 8, 2023",
+  },
+  {
+    id: 8,
+    title: "1914 translation by H. Rackham",
+    details:
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally",
+    date: "Jan 9, 2023",
+  },
+];
+
+export default notes;
